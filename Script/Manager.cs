0d8fb45f2d2769a46c6fcdd2250d9767c3e852f1
Fymyte <34305318+Fymyte@users.noreply.github.com>
2018-01-24 11:21:14 +0100

    24/01
    ajout des premier script de déplacement

---
+++ Script/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+	public GameObject Spawn;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log("Joined Lobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Joined Failed");
+		PhotonNetwork.CreateRoom("room");
+	}
+
+	void OnJoinedRoom()
+	{
+		Debug.Log("Joined");
+		Instantiate(Spawn);
+	}
+}
