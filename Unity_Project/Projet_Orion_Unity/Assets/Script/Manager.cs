53d8508f23b1a75bf28d14588a7f3793e290f898
Fymyte <34305318+Fymyte@users.noreply.github.com>
2018-03-14 15:55:32 +0100

    add test_snake_mouvement

--- Unity_Project/Projet_Orion_Unity/Assets/Script/Manager.cs
+++ Unity_Project/Projet_Orion_Unity/Assets/Script/Manager.cs
@@ -63,7 +63,7 @@ public class Manager : MonoBehaviour
 	void OnJoinedRoom()
 	{
 		Debug.Log("Joined");
-		Snake_object = PhotonNetwork.Instantiate(Snake, Vector3.up, Quaternion.Euler(0,0,0), 0);
+		Snake_object = PhotonNetwork.Instantiate(Snake, new Vector3(0,0,0), Quaternion.Euler(0,0,0), 0);
 		Snake_object.GetComponent<Snake_mouvement>().enabled = true;
 //		Snake_object.SetActive(false);
 //		Snake_object.SetActive(true);
